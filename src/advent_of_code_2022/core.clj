9eeb569f79468141110f162eea45334ee2fe03eb
Jonathan James <jonathan.james@fundingcircle.com>
2022-12-03 18:02:17 +0000

    Day 3

--- src/advent_of_code_2022/core.clj
+++ src/advent_of_code_2022/core.clj
@@ -1,7 +1,8 @@
 (ns advent-of-code-2022.core
   (:gen-class)
   (:require [advent-of-code-2022.one :as one]
-            [advent-of-code-2022.two :as two]))
+            [advent-of-code-2022.two :as two]
+            [advent-of-code-2022.three :as three]))
 
 (defn -main
   "Advent of Code 2022"
@@ -12,4 +13,7 @@
          "Second:" one/second-result)
     "2" (println
          "First:" two/first-result
-         "Second:" two/second-result)))
+         "Second:" two/second-result)
+    "3" (println
+         "First:" three/first-result
+         "Second:" three/second-result)))
---
+++ src/advent_of_code_2022/three.clj
@@ -0,0 +1,30 @@
+(ns advent-of-code-2022.three
+  (:require [clojure.string :as str]))
+
+(def data (str/split-lines (slurp "resources/three.txt")))
+
+(defn score
+  [char]
+  (let [code (int char)]
+    (if (> code 96) (- code 96) (+ (- code 64) 26))))
+
+(def first-result
+  (apply + (map (fn [s] 
+                  (loop [half (/ (count s) 2)
+                         [head & tail] (seq s)]
+                    (if (>= (str/last-index-of s head) half)
+                      (score head)
+                      (recur half tail))))
+                data)))
+
+(def second-result
+  (loop [[line1 line2 line3 & rest] data
+         total 0]
+    (let [next-total (loop [[head & tail] line1]
+                       (if (and (str/includes? line2 (str head))
+                                (str/includes? line3 (str head)))
+                         (+ total (score head))
+                         (recur tail)))]
+      (if (< (count rest) 3)
+        next-total
+        (recur rest next-total)))))
