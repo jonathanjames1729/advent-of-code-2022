0ec5f84335626a7f6bacdec7e95504ec7b12d958
Jonathan James <jonathan.james@fundingcircle.com>
2022-12-04 16:47:14 +0000

    Add option to display all results

--- src/advent_of_code_2022/core.clj
+++ src/advent_of_code_2022/core.clj
@@ -2,18 +2,22 @@
   (:gen-class)
   (:require [advent-of-code-2022.one :as one]
             [advent-of-code-2022.two :as two]
-            [advent-of-code-2022.three :as three]))
+            [advent-of-code-2022.three :as three]
+            [clojure.string :as str]))
+
+(defn run
+  [n]
+  (let [[x y] (case n
+                1 (list one/first-result one/second-result)
+                2 (list two/first-result two/second-result)
+                3 (list three/first-result three/second-result)
+                (list 0 0))]
+    (format "%3d. First: %6d Second: %6d" n x y)))
 
 (defn -main
   "Advent of Code 2022"
   [& args]
-  (case (first args)
-    "1" (println
-         "First:" one/first-result
-         "Second:" one/second-result)
-    "2" (println
-         "First:" two/first-result
-         "Second:" two/second-result)
-    "3" (println
-         "First:" three/first-result
-         "Second:" three/second-result)))
+  (let [N (if (empty? args) 0 (Integer/parseInt (first args)))]
+    (println (if (< N 1) 
+               (str/join "\n" (map run (range 1 4)))
+               (run N)))))
