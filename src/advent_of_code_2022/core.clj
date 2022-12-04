d398620737e89300391b99d606d6a8dcfa4c9c8e
Jonathan James <jonathan.james@fundingcircle.com>
2022-12-04 16:47:57 +0000

    Day 4

--- src/advent_of_code_2022/core.clj
+++ src/advent_of_code_2022/core.clj
@@ -3,6 +3,7 @@
   (:require [advent-of-code-2022.one :as one]
             [advent-of-code-2022.two :as two]
             [advent-of-code-2022.three :as three]
+            [advent-of-code-2022.four :as four]
             [clojure.string :as str]))
 
 (defn run
@@ -11,6 +12,7 @@
                 1 (list one/first-result one/second-result)
                 2 (list two/first-result two/second-result)
                 3 (list three/first-result three/second-result)
+                4 (list four/first-result four/second-result)
                 (list 0 0))]
     (format "%3d. First: %6d Second: %6d" n x y)))
 
@@ -19,5 +21,5 @@
   [& args]
   (let [N (if (empty? args) 0 (Integer/parseInt (first args)))]
     (println (if (< N 1) 
-               (str/join "\n" (map run (range 1 4)))
+               (str/join "\n" (map run (range 1 5)))
                (run N)))))
---
+++ src/advent_of_code_2022/four.clj
@@ -0,0 +1,31 @@
+(ns advent-of-code-2022.four
+  (:require [clojure.string :as str]))
+
+(def data (map (fn [x]
+                 (split-at 2 (map (fn [x] (Integer/parseInt x))
+                                  (str/split x #"[-,]"))))
+               (str/split-lines (slurp "resources/four.txt"))))
+
+(defn inside?
+  [[x-lower x-upper] [y-lower y-upper]]
+  (and (>= x-lower y-lower) (<= x-upper y-upper)))
+
+(defn either-inside?
+  [x y]
+  (or (inside? x y) (inside? y x)))
+
+(def first-result
+  (reduce (fn [memo [x y]]
+            (if (either-inside? x y) (inc memo) memo))
+          0
+          data))
+
+(defn disjoint?
+  [[x-lower x-upper] [y-lower y-upper]]
+  (or (> x-lower y-upper) (> y-lower x-upper)))
+
+(def second-result
+  (reduce (fn [memo [x y]]
+            (if (disjoint? x y) memo (inc memo)))
+          0
+          data))
