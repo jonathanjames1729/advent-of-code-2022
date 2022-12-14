e7e2c3b8418f5c6bbb319f96e72c44d0cea6dfe9
Jonathan James <jonathan.james@fundingcircle.com>
2022-12-14 22:04:24 +0000

    Day 8 (Part 2)

--- src/advent_of_code_2022/eight.clj
+++ src/advent_of_code_2022/eight.clj
@@ -74,4 +74,87 @@
                           (inc subtotal)
                           subtotal)))))))) 
 
-(def second-result 0)
+(defn- look-right
+  [trees]
+  (map (fn [line]
+         (loop [result '()
+                [tree & trees-rightward] line]
+           (if (empty? trees-rightward)
+             (reverse (cons 0 result))
+             (let [trees-split (split-with (partial > tree)
+                                          trees-rightward)]
+               (recur (cons (+ (count (first trees-split))
+                               (if (empty? (last trees-split)) 0 1))
+                            result)
+                      trees-rightward)))))
+       trees))
+
+(defn- look-down
+  [trees]
+  (loop [result '()
+         [line & trees-downward] trees]
+    (if (empty? trees-downward)
+      (reverse (cons (repeat (count line) 0) result))
+      (recur (cons (loop [result2 (repeat (count line) 0)
+                          [line2 & trees-downward2] trees-downward
+                          index 1]
+                     (if (empty? trees-downward2)
+                       (map (fn [tree]
+                              (if (zero? tree) index tree))
+                            result2)
+                       (recur (loop [result3 '()
+                                     [tree & trees-rightward] result2
+                                     [tree1 & trees-rightward1] line
+                                     [tree2 & trees-rightward2] line2]
+                                (let [result4 (cons (if (and (zero? tree)
+                                                             (>= tree2 tree1))
+                                                      index
+                                                      tree)
+                                                    result3)]
+                                  (if (empty? trees-rightward)
+                                    (reverse result4)
+                                    (recur result4
+                                           trees-rightward
+                                           trees-rightward1
+                                           trees-rightward2))))
+                              trees-downward2
+                              (inc index))))
+                   result)
+             trees-downward))))
+
+(defn- multiply
+  [trees1 trees2]
+  (loop [result '()
+         [line1 & trees-downward1] trees1
+         [line2 & trees-downward2] trees2]
+    (let [running-result (cons (loop [line-result '()
+                                      [tree1 & trees-rightward1] line1
+                                      [tree2 & trees-rightward2] line2]
+                                 (let [running-line-result (cons (* tree1 tree2)
+                                                                 line-result)]
+                                   (if (or (empty? trees-rightward1)
+                                           (empty? trees-rightward2))
+                                     (reverse running-line-result)
+                                     (recur running-line-result
+                                            trees-rightward1
+                                            trees-rightward2))))
+                               result)]
+      (if (or (empty? trees-downward1)
+              (empty? trees-downward2))
+        (reverse running-result)
+        (recur running-result
+               trees-downward1
+               trees-downward2)))))
+
+(defn- look-right-and-down
+  [trees]
+  (multiply (look-right trees) (look-down trees)))
+
+(defn- look-left-and-up
+  [trees]
+  (flip (look-right-and-down (flip trees))))
+
+(def second-result
+  (apply max (map (fn [line] (apply max line))
+                  (multiply (look-right-and-down data)
+                            (look-left-and-up data)))))
